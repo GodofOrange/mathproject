056034387d4a051037c661805fb7de31e838f048
GodofOrange <wochidoubao123@163.com>
2019-08-26 20:29:54 +0800

    Initial commit

---
+++ src/main/java/org/just/computer/mathproject/MathprojectApplication.java
@@ -0,0 +1,13 @@
+package org.just.computer.mathproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathprojectApplication.class, args);
+    }
+
+}
