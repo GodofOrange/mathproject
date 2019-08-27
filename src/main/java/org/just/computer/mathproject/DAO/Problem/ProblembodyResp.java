50bcf3de04fb15d6b15dda6631e59f1a2130d887
bellsz <2477034264@qq.com>
2019-08-27 21:23:16 +0800

    第二次提交

--- src/main/java/org/just/computer/mathproject/DAO/Problem/ProblembodyResp.java
+++ src/main/java/org/just/computer/mathproject/DAO/Problem/ProblembodyResp.java
@@ -4,6 +4,6 @@ import org.just.computer.mathproject.Entity.Problem.Problembody;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProblembodyResp extends JpaRepository<Problembody,Integer> {
-    Problembody findByIdEquals(Integer id);
+    Problembody findByProblemsetidEquals(Integer id);
 
 }
--- src/main/java/org/just/computer/mathproject/Service/Problem/ProblemsetLabelService.java
+++ src/main/java/org/just/computer/mathproject/Service/Problem/ProblemsetLabelService.java
@@ -1,13 +1,34 @@
 package org.just.computer.mathproject.Service.Problem;
 
 import org.just.computer.mathproject.DAO.Problem.ProblemsetResp;
+import org.just.computer.mathproject.DAO.Problem.ProblemsetlabelResp;
+import org.just.computer.mathproject.Entity.Problem.ProblemLabel;
+import org.just.computer.mathproject.Entity.Problem.ProblemsetLabel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProblemsetLabelService {
     @Autowired
-    ProblemsetResp problemsetResp;
+    ProblemsetlabelResp problemsetlabelResp;
 
+    public List<ProblemsetLabel> getAllProblemsetLabel() {
+        return problemsetlabelResp.findAll();
+    }
+    public ProblemsetLabel getProblemsetLableById(Integer id){
+        return problemsetlabelResp.findByIdEquals(id);
+    }
 
+    public void addProblemsetLabel(Integer problemsetid,Integer problemlabelid) {
+        ProblemsetLabel problemsetLabel =new ProblemsetLabel();
+        problemsetLabel.setProblemsetid(problemsetid);
+        problemsetLabel.setProblemlabelid(problemlabelid);
+        problemsetlabelResp.save(problemsetLabel);
+    }
+
+    public void deleteProblemsetLabelById(Integer id){
+        problemsetlabelResp.deleteById(id);
+    }
 }
