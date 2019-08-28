0f58a4318adcee1f2fcb7e452c5d12722273cd5e
GodofOrange <wochidoubao123@163.com>
2019-08-28 10:49:23 +0800

    Merge branch 'master' of https://github.com/GodofOrange/mathproject
    # Conflicts:
    #	README.md

--- src/main/java/org/just/computer/mathproject/Config/WebSecurityConfig.java
+++ src/main/java/org/just/computer/mathproject/Config/WebSecurityConfig.java
@@ -1,17 +1,31 @@
 package org.just.computer.mathproject.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.just.computer.mathproject.Service.User.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -41,6 +55,51 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginProcessingUrl("/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        Object principal = authentication.getPrincipal();
+                        httpServletResponse.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        httpServletResponse.setStatus(200);
+                        Map<String,Object> map = new ConcurrentHashMap<>();
+                        map.put("status",200);
+                        map.put("msg",principal);
+                        ObjectMapper om = new ObjectMapper();
+                        out.write(om.writeValueAsString(map));
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        httpServletResponse.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        httpServletResponse.setStatus(401);
+                        Map<String,Object> map = new ConcurrentHashMap<>();
+                        map.put("status",401);
+                        if(e instanceof LockedException){
+                            map.put("msg","账户被锁定，登陆失败");
+                        } else if (e instanceof BadCredentialsException){
+                            map.put("msg","账户密码输入错误");
+                        } else if (e instanceof DisabledException){
+                            map.put("msg","账户被禁用，登陆失败");
+                        } else if (e instanceof AccountExpiredException){
+                            map.put("msg","账户已经过期，登陆失败");
+                        } else if (e instanceof CredentialsExpiredException){
+                            map.put("msg","密码已经过期，登陆失败");
+                        }else {
+                            map.put("msg","登陆失败");
+                        }
+                        ObjectMapper om = new ObjectMapper();
+                        out.write(om.writeValueAsString(map));
+                        out.flush();
+                        out.close();
+                    }
+                }).permitAll()
                 .and()
                 .csrf().disable();
     }
