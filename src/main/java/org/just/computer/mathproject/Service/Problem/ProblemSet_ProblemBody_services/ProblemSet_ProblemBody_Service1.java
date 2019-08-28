a9aeb1b7cfd273ebe119092c6ebdb2bacd945735
bellsz <2477034264@qq.com>
2019-08-28 10:49:45 +0800

    第二次提交

---
+++ src/main/java/org/just/computer/mathproject/Service/Problem/ProblemSet_ProblemBody_services/ProblemSet_ProblemBody_Service1.java
@@ -0,0 +1,30 @@
+package org.just.computer.mathproject.Service.Problem.ProblemSet_ProblemBody_services;
+
+import org.just.computer.mathproject.DAO.Problem.ProblembodyResp;
+import org.just.computer.mathproject.DAO.Problem.ProblemsetResp;
+import org.just.computer.mathproject.Entity.Problem.Problembody;
+import org.just.computer.mathproject.Entity.Problem.Problemset;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProblemSet_ProblemBody_Service1 {
+    @Autowired
+    ProblemsetResp problemsetResp;
+    @Autowired
+    ProblembodyResp problembodyResp;
+    public void addNewProblemByUser(String title,String level,String body,String answer,String kind){
+        Problemset problemset = new Problemset();
+        problemset.setEnabled(0);
+        problemset.setLevel(level);
+        problemset.setTitle(title);
+        problemsetResp.save(problemset);
+        problemset = problemsetResp.findByTitleEquals(title);
+        Problembody problembody = new Problembody();
+        problembody.setAnswer(answer);
+        problembody.setProblemsetid(problemset.getId());
+        problembody.setBody(body);
+        problembody.setKind(kind);
+        problembodyResp.save(problembody);
+    }
+}
