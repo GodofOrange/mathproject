a1e1ec83ac880b01b811e7c04fc6dbe754fa12f3
bellsz <2477034264@qq.com>
2019-08-29 11:20:34 +0800

    第二次提交

---
+++ src/main/java/org/just/computer/mathproject/Service/Book/ChapterService.java
@@ -0,0 +1,29 @@
+package org.just.computer.mathproject.Service.Book;
+
+import org.just.computer.mathproject.DAO.Book.ChapterResp;
+import org.just.computer.mathproject.Entity.Book.Chapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChapterService {
+    @Autowired
+    ChapterResp chapterResp;
+
+    public List<Chapter> getAllChapter() {
+        return chapterResp.findAll();
+    }
+
+    public void addChapter(Integer bookid,String title) {
+        Chapter chapter=new Chapter();
+        chapter.setBookid(bookid);
+        chapter.setTitle(title);
+        chapterResp.save(chapter);
+    }
+
+    public void deleteChapterById(Integer id){
+        chapterResp.deleteById(id);
+    }
+}
