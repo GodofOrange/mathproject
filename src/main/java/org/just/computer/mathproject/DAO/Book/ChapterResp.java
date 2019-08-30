d38ea7cac3557f50b2b39708efa39cb6d48f3858
bellsz <2477034264@qq.com>
2019-08-30 09:42:54 +0800

    第二次提交

--- src/main/java/org/just/computer/mathproject/DAO/Book/ChapterResp.java
+++ src/main/java/org/just/computer/mathproject/DAO/Book/ChapterResp.java
@@ -3,5 +3,8 @@ package org.just.computer.mathproject.DAO.Book;
 import org.just.computer.mathproject.Entity.Book.Chapter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChapterResp extends JpaRepository<Chapter,Integer> {
+    List<Chapter> findAllByBookidEquals(Integer id);
 }
