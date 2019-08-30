2c083761c3479317ece4210a0c885f85d81c705d
bellsz <2477034264@qq.com>
2019-08-30 13:16:17 +0800

    第二次提交

--- src/main/java/org/just/computer/mathproject/Service/Article/ArticleService.java
+++ src/main/java/org/just/computer/mathproject/Service/Article/ArticleService.java
@@ -1,10 +1,13 @@
 package org.just.computer.mathproject.Service.Article;
 
 import org.just.computer.mathproject.DAO.Article.ArticleResp;
+import org.just.computer.mathproject.DAO.Users.UserDao;
 import org.just.computer.mathproject.Entity.Article.Article;
+import org.just.computer.mathproject.Entity.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 
@@ -12,16 +15,18 @@ import java.util.List;
 public class ArticleService {
     @Autowired
     ArticleResp articleResp;
-
+    @Autowired
+    UserDao userDao;
     public List<Article> getAllArticle() {
         return articleResp.findAll();
     }
 
-    public void addArticle(String title,String content,Integer userid,Integer enabled) {
+    public void addArticle(Principal pl,String title, String content, Integer userid, Integer enabled) {
         Article article=new Article();
+        User user= userDao.findUserByUsername(pl.getName());
         article.setTitle(title);
         article.setContent(content);
-        article.setUserid(userid);
+        article.setUserid(user.getId());
         article.setEnabled(enabled);
         article.setTime(new Date());
         articleResp.save(article);
